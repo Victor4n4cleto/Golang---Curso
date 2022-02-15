7cc95fa88de6c067f5be86d82c9708436f854a7b
Victor Anacleto <contatoanacletovictor@gmail.com>
2022-02-14 22:28:37 -0300

    Comandos para utilizar no terminal

---
+++ fundamentos/aula-de-comandos/comandos.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Comandos importantes durante o desenvolvimento em Go")
+}
+
+/*
+
+Comandos do Go:
+
++ Comando principal = go
+
++ Comando que discrimina as funções de terminado pedido = go help
+
++ Comando que realiza o download de pacotes quando
+necessario tanto do github quando do diretorio = go get
+
++ Comando para verificar a version do Go instalada = go version
+
++ Comando para obter a documentação do Go offline = godoc -http=:6060
+( assim que executar o comando entrar no navagador e digitar  " localhost:6060" )
+
++ Comando que mostra todas as informações/dependencias/arquivos de instalação do Go = go env
+
++ Comando que verifica determinados erros que podem haver no codigo = go doc cmd/vet ( nome do arquivo a ser verificado )
+
++ Comando que constroe arquivos para execução do compilador = go build
+
++ Comando que compila e executa app no terminal = go run
+
+
+*/
---
+++ fundamentos/primeira-aula/primeiro.go
@@ -0,0 +1,18 @@
+// testando barra de comentarios de unica linha
+package main
+
+/*
+comentários de mais de uma linha
+testando
+*/
+import "fmt"
+
+// A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("Primeiros códigos !")
+}
+
+/*
+1- Priorize códigos legíveis e de fácio entendimento só comente oque valha apena
+2- Evite falar comentários óbvios e priorize comentarios explicativos da função
+*/
